7c3d5671c858f0e171f41b93d8e3d7209a812d8c
Yujie YAO <scyyy5@exmail.nottingham.ac.uk>
2024-12-01 22:15:52 +0000

    refine variable names

--- 2024/day1/day1_part2.py
+++ 2024/day1/day1_part2.py
@@ -1,19 +1,19 @@
 from collections import defaultdict
 
-left_dic = defaultdict(int)
-right_dic = defaultdict(int)
+left_dict = defaultdict(int)
+right_dict = defaultdict(int)
 
 with open("inputs/day1.txt", 'r') as file:
     for line in file:
         left, right = map(int, line.strip().split())
-        left_dic[left] += 1
-        right_dic[right] += 1
+        left_dict[left] += 1
+        right_dict[right] += 1
 
 sum_similarity = 0
 
-for left in left_dic.keys():
-    num_left = left_dic[left]
-    num_right = right_dic[left]
+for left in left_dict.keys():
+    num_left = left_dict[left]
+    num_right = right_dict[left]
     sum_similarity += num_left*left*num_right
 
 
