6df4cc0f4923bc86bb620e7756326f13f1e6c2c5
Yujie YAO <scyyy5@exmail.nottingham.ac.uk>
2024-12-07 17:11:59 +0000

    simplify logic and improve performance

--- 2024/day6/day6_part2.py
+++ 2024/day6/day6_part2.py
@@ -11,6 +11,32 @@ with open("2024/inputs/day6.txt") as file:
             start_j = grid_line.index('^')
         grid.append(grid_line)
 
+def count_paths(i, j, grid, direction):
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    direction = directions[direction]
+    visited = set()
+    visited.add((i, j))
+    leave = False
+    while True:
+        i += direction[0]
+        j += direction[1]
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
+            leave = True
+            break
+        if grid[i][j] == "#":
+            break
+        visited.add((i, j))
+    return visited, i-direction[0], j-direction[1], leave
+
+def get_paths(i, j, grid, direction):
+    paths = set()
+    while True:
+        visited, i, j, leave = count_paths(i, j, grid, direction)
+        paths = paths.union(visited)
+        direction = (direction + 1) % 4
+        if leave:
+            break
+    return paths
 
 def check_loop(i, j, new_obstacle_i, new_obstacle_j):
     visited = set()
@@ -37,15 +63,14 @@ def check_loop(i, j, new_obstacle_i, new_obstacle_j):
 
 
 ans = 0
-
 i = start_i
 j = start_j
 direction = 0
 
-for i in range(len(grid)):
-    for j in range(len(grid[0])):
-        print(i, j)
-        sum += check_loop(start_i, start_j, i, j)
+paths = get_paths(i, j, grid, direction)
+
+for (i,j) in paths:
+    ans += check_loop(start_i, start_j, i, j)
         
 print(ans)
 
